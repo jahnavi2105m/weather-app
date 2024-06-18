822e85684c3a7160a00d2021ff63ed78c879182c
Jahnavi Malhotra <j7malhot@uwaterloo.ca>
2024-06-18 15:36:58 -0400

    update

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ function fetchWeather() {
     let searchInput = document.getElementById("search").value;
     const weatherDataSection = document.getElementById("weather-data");
     weatherDataSection.style.display = "block";
-    //key
+    
+    if (searchInput == "") {
+        weatherDataSection.innerHTML = `
+        <div>
+          <h2>Input is Empty!</h2>
+          <p>Please try again with a valid <u>city name</u>.</p>
+        </div>
+        `;
+        return;
+      }
+
+      async function getLonAndLat() {
+
+      }
+
+      async function getWeatherData(lon, lat) {
+
+      }
 }
+
