6fae4c6f69104b08a2bbe79d1b68e2f9f1aa80cf
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-07-12 20:24:57 -0400

    test commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ async function fetchWeather() {
     let searchInput = document.getElementById("search").value;
     const weatherDataSection = document.getElementById("weather-data");
     weatherDataSection.style.display = "block";
-    //api key hidden
-
+    //api key
+//test comment
     if (searchInput == "") {
         weatherDataSection.innerHTML = `
         <div>
